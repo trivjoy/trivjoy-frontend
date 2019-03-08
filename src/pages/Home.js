c6c8c420784b98b8dbaaa228b15b73b6dc210f00
Sakti Dewantoro <saktyd@gmail.com>
2019-03-08 19:07:02 +0700

    add logo images for help page and create little bit content on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,46 @@ import Footer from '../components/Footer'
 import styled from 'styled-components'
 
 const HomeContent = styled.div``
-
+const LogoStyle = styled.img`
+  height: 38px;
+  width: 38px;
+`
+const YouShouldGoTravelling = styled.p`
+  display: flex;
+  font-size: 25px;
+  margin: 0 auto;
+`
 const Home = () => {
   return (
     <div>
       <HeaderHome />
-      <HomeContent />
+      <HomeContent>
+        <div>
+          <p>First,</p>
+          <p>Why You Should Go To Travelling?</p>
+
+          <div>
+            <YouShouldGoTravelling>
+              <LogoStyle src="/assets/logo/checklist-home.png" alt="" />
+              Doesn’t have friends for traveling?
+            </YouShouldGoTravelling>
+
+            <YouShouldGoTravelling>
+              <LogoStyle src="/assets/logo/checklist-home.png" alt="" />
+              Do you want to have new experiences?
+            </YouShouldGoTravelling>
+
+            <YouShouldGoTravelling>
+              <LogoStyle src="/assets/logo/checklist-home.png" alt="" />
+              Arranging trips, does it have to be so hectic?
+            </YouShouldGoTravelling>
+          </div>
+        </div>
+        <div>
+          <p>Second,</p>
+          <div />
+        </div>
+      </HomeContent>
       <Footer />
     </div>
   )
