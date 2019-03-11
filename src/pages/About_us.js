8d7a2a5d87bc5b24f438d1549e663a42fe5ca28f
Bara Limbong <baralimbong@gmail.com>
2019-03-11 19:12:36 +0700

    create about us pages

--- src/pages/About_us.js
+++ src/pages/About_us.js
@@ -4,39 +4,36 @@ import Footer from '../components/Footer'
 import styled from 'styled-components'
 
 const Content = styled.div`
-  justify-content: center;
-  align-items: center;
+  display: flex;
+  flex-direction: row;
   flex: 1;
 `
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
+  min-height: 100vh;
 `
-const TextTitle = styled.div`
+const StyledH1 = styled.h1`
+  text-align: center;
+  font-size: 30px;
+`
+const StyledHelpParagraph = styled.p`
   justify-content: center;
-  align-items: center;
-`
-const FirstDiscribeContent = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 30px;
-  height: 100px;
-  font-size: 20px;
-`
-const ParagraphCard = styled.p`
-  margin: -70px 10px 10px 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
-const TagDivParagraph = styled.div`
-  display: flex;
-`
-const Paragraph = styled.p`
-  margin-left: 500px;
-  margin-right: 20px;
   text-align: justify;
+  font-size: 15px;
+  flex-direction: column;
+  padding: 0 100px;
+  text-rendering: auto;
+`
+const InformationColumn = styled.div`
+  font-size: 15px;
+  width: 404px;
+  height: 25px;
+  left: 40px;
+  top: 303px;
+  font-style: normal;
+  font-weight: normal;
+  line-height: normal;
 `
 
 const About_us = () => {
@@ -44,35 +41,38 @@ const About_us = () => {
     <Container>
       <Header />
       <Content>
-        <TextTitle>
-          <nav>
-            <p>General Information about our company</p>
-          </nav>
-          <FirstDiscribeContent>
-            <b>About US</b>
-          </FirstDiscribeContent>
-          <ParagraphCard>General Information about our company</ParagraphCard>
-          <TagDivParagraph>
-            <Paragraph>
+        <InformationColumn>
+          <h1>Information</h1>
+          <p>Information about our company</p>
+        </InformationColumn>
+        <div>
+          <StyledH1>About Us</StyledH1>
+          <StyledHelpParagraph>
+            <p>
               Trivjoy is a social travel platfform with a goal to inspire you to
               travel more and to experience new adventures. Our mission is to
               make it easy for travelers to connect and to travel the world
               together. We believe that traveling is more fun when you have
               someone with whom you can share your travel experience and
-              adventures. TripGiraffe thrives to create an extensive database of
-              travelers with plans to travel to all the places one can possibly
-              think of. Getting the right people to travel together has never
-              been easier! Creating a trip on Trivjoy gives you an option to
-              specify in detail all the information about your trip together
-              with your expectations. Are you looking for someone traveling on a
-              budget, maybe for someone with whom you could split cost of
-              accommodation? Or maybe you are looking for someone who is
-              planning to do work&travel? Either way, TripGiraffe will have you
-              covered. Join an existing trip or create your own one and go
-              expore together!
-            </Paragraph>
-          </TagDivParagraph>
-        </TextTitle>
+              adventures.
+            </p>
+            <p>
+              TripGiraffe thrives to create an extensive database of travelers
+              with plans to travel to all the places one can possibly think of.
+              Getting the right people to travel together has never been easier!
+              Creating a trip on Trivjoy gives you an option to specify in
+              detail all the information about your trip together with your
+              expectations.
+            </p>
+            <p>
+              Are you looking for someone traveling on a budget, maybe for
+              someone with whom you could split cost of accommodation? Or maybe
+              you are looking for someone who is planning to do work&travel?
+              Either way, TripGiraffe will have you covered. Join an existing
+              trip or create your own one and go expore together!
+            </p>
+          </StyledHelpParagraph>
+        </div>
       </Content>
       <Footer />
     </Container>
